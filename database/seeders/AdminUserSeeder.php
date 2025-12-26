fd7906167f18e914f6f8e24963b4cc4ed5f2de6f
Nicolas Moron <nicolasmoron15@gmail.com>
2025-12-25 22:05:54 -0300

    Admin Seeder

---
+++ database/seeders/AdminUserSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
+
+class AdminUserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::create([
+            'name' => 'Adminstrador',
+            'email' => 'admin@example.com',
+            'password' => Hash::make('admin1234'),
+        ]);
+    }
+}
